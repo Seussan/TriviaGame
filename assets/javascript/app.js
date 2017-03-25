61643e2eb0b76a98ee73181d2f96a5920c29fcfe
Seussan <Seussan>
2017-03-25 08:29:55 -0400

    Updated files.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,102 +1,198 @@
-// $(document).ready(function() {
+var timer;
 
-	var trivia = {
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var currentQuestion = 0;
+var questionNumber = 0;
 
-		questions: [
-		{
-			category: "Metal Music",
-			question: "What is the name of Iron Maiden's mascot?",
-			choices: [ "Mickey", "Oscar", "Eddie", "Mighty" ],
-			answer: "3"
-		},
-		{
-			category: "Metal Music",
-			question: "Richie Blackmore from Deep Purple and Rainbow, spends his time playing...",
-			choices: [ "Blistering Riffs", "Renaissance Guitar", "Banjo Goodness", "Autoharp" ],
-			answer: "2"
-		},
-		{
-			category: "Metal Music",
-			question: "Procol Harum is best know for their song...",
-			choices: [ "The Prodigal Stranger", "Wasted Years", "A Whiter Shade of Pale", "Conquistador" ],
-			answer: "3"
-		}
-		],
+var currentTime = 0;
 
-		displayCategory: function(number) {
+var trivia = {
 
-			$(".category").html("Category: " + trivia.questions[number].category);
-		},
+	questions: [
+	{
+		category: "Metal Music",
+		question: "What is the name of Iron Maiden's mascot?",
+		choices: [ "Mickey", "Oscar", "Eddie", "Mighty" ],
+		answer: 3
+	},
+	{
+		category: "Metal Music",
+		question: "Richie Blackmore from Deep Purple and Rainbow, spends his time these days playing...",
+		choices: [ "Blistering Riffs", "Renaissance Guitar", "Banjo Goodness", "Autoharp" ],
+		answer: 2
+	},
+	{
+		category: "Metal Music",
+		question: "Your favorite metal band is...",
+		choices: [ "Metallica", "Iron Maiden", "Black Sabbath", "Motley Crue" ],
+		answer: 2
+	},
+	{
+		category: "Metal Music",
+		question: "Procol Harum is best know for their song...",
+		choices: [ "The Prodigal Stranger", "Wasted Years", "A Whiter Shade of Pale", "Conquistador" ],
+		answer: 3
+	}
+	],
 
-		displayQuestion: function(number) {
+	countdown: function() {
 
-			$(".question").html(trivia.questions[number].question);
-		},
+		currentTime--;
 
-		displayChoices: function(number) {
+		$("#timer").html("Time Remaining: " + currentTime + " seconds");
 
-			for (var i = 0; i < trivia.questions[number].choices.length; i++) {
+		$(".answerBtn").click(function() {
+			console.log("click inside countdown");
+			trivia.checkAnswer(questionNumber);
+		});
+		
+		if (currentTime === 0) {
 
-				var choiceButton = $("<button>");
+			trivia.checkAnswer(questionNumber);
+		};
+	},
 
-    		// Each button is given the appropriate classes for CSS.
+	summary: function() {
+
+		clearInterval(timer);
+
+		$(".trivia").html("<h2>Here are your results!</h2>");
+		$(".trivia").append("<h3>Correct Answers: " + correct + "</h3>");
+		$(".trivia").append("<h3>Incorrect Answers: " + incorrect + "</h3>");
+		$(".trivia").append("<h3>Unanswered: " + unanswered + "</h3>");
+		$(".trivia").append("<br><button class='replayBtn btn btn-warning'>Play Again?</button>");
+	},
+
+	displayCategory: function(number) {
+
+		$(".trivia").html("<h2>Category: " + trivia.questions[number].category + "</h2>");
+	},
+
+	displayQuestion: function(number) {
+
+		$(".trivia").append("<h2>" + trivia.questions[number].question + "</h2><br><br>");
+	},
+
+	displayChoices: function(number) {
+
+		for (var i = 0; i < trivia.questions[number].choices.length; i++) {
+
+			var choiceButton = $("<button>");
+
+    		// Button is given the appropriate classes for CSS.
     		choiceButton.addClass("answerBtn btn-primary");
 
-    		// Each button is assigned a value for comparing the right answer.
+    		// Button is assigned a value for comparing the right answer.
     		choiceButton.attr("value", i+1);
 
  			// Put the answer on the button.
  			choiceButton.html(trivia.questions[number].choices[i]);	
 
-    		// Each button is added to the page.
-    		$("#buttons").append(choiceButton);
+    		// Button is added to the page.
+    		$(".trivia").append(choiceButton);
     	}
     },
 
     checkAnswer: function(number) {
 
-    	$(".answerBtn").click(function() {
+    	if (currentTime === 0) {
 
-    		if (trivia.questions[number].answer == parseInt($(this).attr("value"))) {	
+    		unanswered++;
+
+    		$('.trivia').html("<h2>Time's Up!</h2>");
+    		$('.trivia').append("<br><h2>Correct Answer: " + trivia.questions[number].choices[trivia.questions[number].answer-1] + "</h2>");
+    	}    	
+
+    	clearInterval(timer);
+
+    	// $(".answerBtn").click(function() {
+    		// console.log("click inside checkAnswer");
+    		// if (trivia.questions[number].answer == parseInt($(this).attr("value"))) {	
+
+    		console.log(trivia.questions[number].answer);
+    		console.log($(this).attr("value"));
+    		console.log(parseInt($(this).attr("value")));
+    		if (trivia.questions[number].answer == $(this).attr("value")) {	
+
 				// Winner!!
-				$('#buttons').empty();
+				$('.trivia').html("<h2>That's Correct!!</h2>");
+				$('.trivia').append('<br><img src="assets/images/winner-winner.jpeg">');
 
-				$('#buttons').append('<img src="assets/images/winner-winner.jpeg">');
-				setTimeout(function() { $("#buttons").hide(); }, 2000);
-
-				// trivia.start(30);
+				correct++;
 			}
 			else {
-				// console.log("Not Winner!!")
-			}
-		});
+
+				// Not A Winner!!
+				$('.trivia').html("<h2>That's NOT Correct!!</h2>");
+				$('.trivia').append("<br><h2>The correct answer is: " + trivia.questions[number].choices[trivia.questions[number].answer-1] + "</h2>");
+				$('.trivia').append('<br><img src="assets/images/not-a-winner.jpg">');
+
+				incorrect++;	
+			};
+
+		// });
+
+    	if (currentQuestion < trivia.questions.length) {
+
+    		setTimeout(trivia.start, 3000);
+    	}
+    	else {
+
+    		setTimeout(trivia.summary, 3000);
+    	}
     },
 
     selectQuestion: function() {
-    	return(Math.floor(Math.random() * trivia.questions.length));
+
+    	return(currentQuestion++);
+
+    	// return(Math.floor(Math.random() * trivia.questions.length));
     },
 
-    start: function(time) {
+    start: function() {
 
-    	$("#start").hide();
+    	currentTime = 5; //seconds
 
-    	$("#timer").html("Time Remaining: " + time + " seconds");
+    	clearInterval(timer);
+
+    	timer = setInterval(trivia.countdown, 1000);
+
+    	$("#timer").html("Time Remaining: " + currentTime + " seconds");
 
     	questionNumber = trivia.selectQuestion();
-
     	trivia.displayCategory(questionNumber);
     	trivia.displayQuestion(questionNumber); 
     	trivia.displayChoices(questionNumber);
-    	trivia.checkAnswer(questionNumber);
     }
 };
 
 window.onload = function() {
 
-	$("#start").click(function() {
+	$(".startBtn").click(function() {
 
-		trivia.start(30);
+		// timer = setInterval(trivia.countdown, 1000);
+
+		trivia.start();
 	});
 };
 
-// };
+$(document).on("click", ".answerBtn", function() {
+
+	trivia.checkAnswer(questionNumber);
+});
+
+$(document).on("click", ".replayBtn", function() {
+
+	timer = 5;
+
+	correct = 0;
+	incorrect = 0;
+	unanswered = 0;
+	currentQuestion = 0;
+
+	currentTime = 0;
+
+	trivia.start();
+});
